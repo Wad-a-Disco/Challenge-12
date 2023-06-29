c5a3fa0252ee8c8d3ab35262fa95d8c00bd424bd
Wade Desco <descowade@gmail.com>
2023-06-28 22:47:49 -0700

    Finalized code

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
 
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'your_mysql_username',
+  user: 'root',
   password: 'your_mysql_password',
-  database: 'employee_db',
+  database: 'employee_tracker',
 });
 
 // Connect to the database
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected to the database.');
-
-  // Call the function to start the application prompts
+  console.log('Connected to the employee_tracker database.\n');
   startPrompt();
 });
 
-// Function to start the initial prompt
+// Function to start the application prompt
 function startPrompt() {
   inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'action',
-        message: 'What would you like to do?',
-        choices: [
-          'View all departments',
-          'View all roles',
-          'View all employees',
-          'Add a department',
-          'Add a role',
-          'Add an employee',
-          'Update an employee role',
-          'Exit',
-        ],
-      },
-    ])
-    .then((answers) => {
-      // Based on the user's choice, call the corresponding function
-      switch (answers.action) {
+    .prompt({
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'View the total utilized budget of a department',
+        'Delete a department',
+        'Delete a role',
+        'Delete an employee',
+        'Exit',
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
         case 'View all departments':
           viewDepartments();
           break;
@@ -63,12 +63,25 @@ function startPrompt() {
         case 'Update an employee role':
           updateEmployeeRole();
           break;
+        case 'View the total utilized budget of a department':
+          viewDepartmentBudget();
+          break;
+        case 'Delete a department':
+          deleteDepartment();
+          break;
+        case 'Delete a role':
+          deleteRole();
+          break;
+        case 'Delete an employee':
+          deleteEmployee();
+          break;
         case 'Exit':
+          console.log('Exiting the application.');
           connection.end();
-          console.log('Connection closed.');
           break;
         default:
-          console.log('Invalid choice.');
+          console.log('Invalid action. Please try again.');
+          startPrompt();
       }
     })
     .catch((error) => {
@@ -76,46 +89,305 @@ function startPrompt() {
     });
 }
 
-// Implement the rest of the functions for each action, such as viewDepartments(), viewRoles(), addDepartment(), etc.
-
 // Function to view all departments
 function viewDepartments() {
-  // TODO: Implement SQL query to fetch all departments from the database
-  // Execute the query and display the results
+  const query = 'SELECT * FROM department';
+
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+
+    console.log('\nDepartments:\n');
+    console.table(res);
+
+    startPrompt();
+  });
 }
 
 // Function to view all roles
 function viewRoles() {
-  // TODO: Implement SQL query to fetch all roles from the database
-  // Execute the query and display the results
+  const query = `
+    SELECT role.id, role.title, role.salary, department.name AS department
+    FROM role
+    INNER JOIN department ON role.department_id = department.id
+  `;
+
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+
+    console.log('\nRoles:\n');
+    console.table(res);
+
+    startPrompt();
+  });
 }
 
 // Function to view all employees
 function viewEmployees() {
-  // TODO: Implement SQL query to fetch all employees from the database
-  // Execute the query and display the results
+  const query = `
+    SELECT 
+      employee.id, 
+      employee.first_name, 
+      employee.last_name, 
+      role.title AS job_title, 
+      department.name AS department, 
+      role.salary, 
+      CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id
+  `;
+
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+
+    console.log('\nEmployees:\n');
+    console.table(res);
+
+    startPrompt();
+  });
 }
 
 // Function to add a department
 function addDepartment() {
-  // TODO: Implement inquirer prompt to get the department details from the user
-  // Retrieve the user input and execute the SQL query to add the department to the database
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'departmentName',
+      message: 'Enter the name of the department:',
+    })
+    .then((answer) => {
+      const query = 'INSERT INTO department SET ?';
+
+      connection.query(query, { name: answer.departmentName }, (err, res) => {
+        if (err) throw err;
+
+        console.log(`\nDepartment "${answer.departmentName}" added successfully!\n`);
+
+        startPrompt();
+      });
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
 }
 
 // Function to add a role
 function addRole() {
-  // TODO: Implement inquirer prompt to get the role details from the user
-  // Retrieve the user input and execute the SQL query to add the role to the database
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'roleTitle',
+        message: 'Enter the title of the role:',
+      },
+      {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'Enter the salary of the role:',
+      },
+      {
+        type: 'input',
+        name: 'departmentId',
+        message: 'Enter the department ID for the role:',
+      },
+    ])
+    .then((answer) => {
+      const query = 'INSERT INTO role SET ?';
+
+      connection.query(
+        query,
+        {
+          title: answer.roleTitle,
+          salary: answer.roleSalary,
+          department_id: answer.departmentId,
+        },
+        (err, res) => {
+          if (err) throw err;
+
+          console.log(`\nRole "${answer.roleTitle}" added successfully!\n`);
+
+          startPrompt();
+        }
+      );
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
 }
 
 // Function to add an employee
 function addEmployee() {
-  // TODO: Implement inquirer prompt to get the employee details from the user
-  // Retrieve the user input and execute the SQL query to add the employee to the database
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: "Enter the employee's first name:",
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: "Enter the employee's last name:",
+      },
+      {
+        type: 'input',
+        name: 'roleId',
+        message: "Enter the employee's role ID:",
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: "Enter the employee's manager ID (if applicable):",
+      },
+    ])
+    .then((answer) => {
+      const query = 'INSERT INTO employee SET ?';
+
+      connection.query(
+        query,
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: answer.roleId,
+          manager_id: answer.managerId || null,
+        },
+        (err, res) => {
+          if (err) throw err;
+
+          console.log(`\nEmployee "${answer.firstName} ${answer.lastName}" added successfully!\n`);
+
+          startPrompt();
+        }
+      );
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
 }
 
-// Function to update an employee's role
+// Function to update an employee role
 function updateEmployeeRole() {
-  // TODO: Implement inquirer prompt to get the employee and new role details from the user
-  // Retrieve the user input and execute the SQL query to update the employee's role in the database
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'employeeId',
+        message: "Enter the employee's ID:",
+      },
+      {
+        type: 'input',
+        name: 'newRoleId',
+        message: "Enter the new role ID for the employee:",
+      },
+    ])
+    .then((answer) => {
+      const query = 'UPDATE employee SET role_id = ? WHERE id = ?';
+
+      connection.query(query, [answer.newRoleId, answer.employeeId], (err, res) => {
+        if (err) throw err;
+
+        console.log(`\nEmployee role updated successfully!\n`);
+
+        startPrompt();
+      });
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
+}
+
+// Function to view the total utilized budget of a department
+function viewDepartmentBudget() {
+  const query = `
+    SELECT 
+      department.id,
+      department.name AS department,
+      SUM(role.salary) AS utilized_budget
+    FROM department
+    LEFT JOIN role ON department.id = role.department_id
+    LEFT JOIN employee ON role.id = employee.role_id
+    GROUP BY department.id, department.name
+  `;
+
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+
+    console.log('\nDepartment Budgets:\n');
+    console.table(res);
+
+    startPrompt();
+  });
+}
+
+// Function to delete a department
+function deleteDepartment() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'departmentId',
+      message: 'Enter the ID of the department you want to delete:',
+    })
+    .then((answer) => {
+      const query = 'DELETE FROM department WHERE id = ?';
+
+      connection.query(query, answer.departmentId, (err, res) => {
+        if (err) throw err;
+
+        console.log(`\nDepartment with ID "${answer.departmentId}" deleted successfully!\n`);
+
+        startPrompt();
+      });
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
+}
+
+// Function to delete a role
+function deleteRole() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'roleId',
+      message: 'Enter the ID of the role you want to delete:',
+    })
+    .then((answer) => {
+      const query = 'DELETE FROM role WHERE id = ?';
+
+      connection.query(query, answer.roleId, (err, res) => {
+        if (err) throw err;
+
+        console.log(`\nRole with ID "${answer.roleId}" deleted successfully!\n`);
+
+        startPrompt();
+      });
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
+}
+
+// Function to delete an employee
+function deleteEmployee() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'employeeId',
+      message: 'Enter the ID of the employee you want to delete:',
+    })
+    .then((answer) => {
+      const query = 'DELETE FROM employee WHERE id = ?';
+
+      connection.query(query, answer.employeeId, (err, res) => {
+        if (err) throw err;
+
+        console.log(`\nEmployee with ID "${answer.employeeId}" deleted successfully!\n`);
+
+        startPrompt();
+      });
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
 }
--- schema.sql
+++ schema.sql
@@ -1,25 +1,64 @@
--- Create the 'department' table
-CREATE TABLE department (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL
-);
+-- Department seed data
+INSERT INTO department (name) VALUES ('Sales');
+INSERT INTO department (name) VALUES ('Engineering');
+INSERT INTO department (name) VALUES ('Marketing');
+INSERT INTO department (name) VALUES ('Finance');
 
--- Create the 'role' table
-CREATE TABLE role (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10, 2) NOT NULL,
-  department_id INT NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES department(id)
-);
+-- Role seed data
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Manager', 80000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Representative', 50000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 70000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Marketing Coordinator', 55000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ('Finance Manager', 90000, 4);
 
--- Create the 'employee' table
-CREATE TABLE employee (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
-);
+-- Employee seed data
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jane', 'Smith', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Mike', 'Johnson', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Williams', 4, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('David', 'Brown', 5, 3);
+
+-- Additional functionality
+
+-- Update employee managers
+UPDATE employee SET manager_id = 2 WHERE id = 3;
+UPDATE employee SET manager_id = 1 WHERE id = 4;
+
+-- View employees by manager
+SELECT 
+  m.id AS manager_id, 
+  m.first_name AS manager_first_name, 
+  m.last_name AS manager_last_name, 
+  e.id AS employee_id, 
+  e.first_name AS employee_first_name, 
+  e.last_name AS employee_last_name 
+FROM employee AS e
+JOIN employee AS m ON e.manager_id = m.id
+ORDER BY m.id;
+
+-- View employees by department
+SELECT 
+  d.id AS department_id, 
+  d.name AS department_name, 
+  e.id AS employee_id, 
+  e.first_name AS employee_first_name, 
+  e.last_name AS employee_last_name 
+FROM employee AS e
+JOIN role AS r ON e.role_id = r.id
+JOIN department AS d ON r.department_id = d.id
+ORDER BY d.id;
+
+-- Delete departments, roles, and employees
+DELETE FROM employee WHERE id = 5;
+DELETE FROM role WHERE id = 5;
+DELETE FROM department WHERE id = 4;
+
+-- View the total utilized budget of a department
+SELECT 
+  d.id AS department_id, 
+  d.name AS department_name, 
+  SUM(r.salary) AS total_budget 
+FROM department AS d
+JOIN role AS r ON d.id = r.department_id
+JOIN employee AS e ON r.id = e.role_id
+GROUP BY d.id;
